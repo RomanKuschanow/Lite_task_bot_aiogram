dd343c93844e3f178bcc821ab1a79731a7b05712
RomanKuschanow <kuschanow@gmail.com>
2022-03-18 18:34:41 +0200

    worked back button

--- bot/handlers/default/new_reminder.py
+++ bot/handlers/default/new_reminder.py
@@ -26,19 +26,22 @@ async def new_reminder(message: Message, state: FSMContext, call_from_back = Fal
     async with state.proxy() as data:
         if not call_from_back:
             data['message'] = list()
+            data['fail'] = 0
+            data['message'].append(message.message_id)
         data['message'].append(bot_message.message_id)
-        data['message'].append(message.message_id)
+        print(data['message'])
 
 
 @dp.message_handler(state=NewReminder.text, content_types=ContentTypes.ANY)
 async def get_reminder_text(message: Message, state: FSMContext, call_from_back = False):
     if message.content_type != 'text':
         text = _(f'Вы прислали мне {message.content_type}, а нужно прислать текст')
-        bot_message = await message.answer(text, reply_markup=get_inline_states_markup())
+        bot_message = await message.answer(text, reply_markup=get_inline_states_markup(True))
         async with state.proxy() as data:
-            await bot.delete_message(message.chat.id, data['message'])
-            data['message'].append(bot_message.message_id)
+            data['fail'] += 1
             data['message'].append(message.message_id)
+            data['message'].append(bot_message.message_id)
+            print(data['message'])
         return
 
     text = _("Теперь выберите дату")
@@ -46,6 +49,13 @@ async def get_reminder_text(message: Message, state: FSMContext, call_from_back 
     if not call_from_back:
         async with state.proxy() as data:
             data['text'] = message.text
+            data['message'].append(message.message_id)
+            for i in range(2 * data['fail']):
+                await bot.delete_message(message.chat.id, data['message'][-2])
+                data['message'].pop(-2)
+
+            data['fail'] = 0
+            print(data['message'])
 
     await NewReminder.date.set()
 
@@ -53,7 +63,7 @@ async def get_reminder_text(message: Message, state: FSMContext, call_from_back 
 
     async with state.proxy() as data:
         data['message'].append(bot_message.message_id)
-        data['message'].append(message.message_id)
+        print(data['message'])
 
 
 @dp.callback_query_handler(simple_cal_callback.filter(), state=NewReminder.date)
@@ -72,7 +82,7 @@ async def get_reminder_date(callback_query: CallbackQuery, callback_data: dict, 
 
         async with state.proxy() as data:
             data['message'].append(bot_message.message_id)
-            data['message'].append(callback_query.message.message_id)
+            print(data['message'])
 
 
 @dp.message_handler(state=NewReminder.time, content_types=ContentTypes.ANY)
@@ -81,16 +91,20 @@ async def get_reminder_date(message, session, user, state: FSMContext):
         text = _(f'Вы прислали мне {message.content_type}, а нужно прислать текст')
         bot_message = await message.answer(text, reply_markup=get_inline_states_markup())
         async with state.proxy() as data:
-            data['message'].append(bot_message.message_id)
+            data['fail'] += 1
             data['message'].append(message.message_id)
+            data['message'].append(bot_message.message_id)
+            print(data['message'])
         return
 
     if not re.match(r'^(\d{2})[\ |\:]?(\d{2})$', message.text):
         text = _('формат не соответсвует')
         bot_message = await message.answer(text, reply_markup=get_inline_states_markup())
         async with state.proxy() as data:
-            data['message'].append(bot_message.message_id)
+            data['fail'] += 1
             data['message'].append(message.message_id)
+            data['message'].append(bot_message.message_id)
+            print(data['message'])
         return
 
     text = ""
@@ -104,11 +118,14 @@ async def get_reminder_date(message, session, user, state: FSMContext):
             text = _('вы ввели несуществующее время')
             bot_message = await message.answer(text, reply_markup=get_inline_states_markup())
             async with state.proxy() as data:
-                data['message'].append(bot_message.message_id)
+                data['fail'] += 1
                 data['message'].append(message.message_id)
+                data['message'].append(bot_message.message_id)
+                print(data['message'])
             return
         text = _(f"Напоминание '{data['text']}' установлено на {data['date']} {match[1]}:{match[2]}")
         data['message'].append(message.message_id)
+        print(data['message'])
 
     await message.answer(text)
 
@@ -125,15 +142,23 @@ async def get_reminder_date(message, session, user, state: FSMContext):
 @dp.callback_query_handler(text="back", state=[NewReminder.date, NewReminder.time])
 async def back(callback_query: CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
-        if data.state == 'NewReminder:date':
-            await NewReminder.time.set()
-            await new_reminder(callback_query.message, state, True)
-        else:
-            await NewReminder.date.set()
-            await get_reminder_text(callback_query.message, state, True)
+        curr_state = data.state
+    if curr_state == 'NewReminder:date':
+        await NewReminder.time.set()
+        await new_reminder(callback_query.message, state, True)
+        async with state.proxy() as data:
+            for i in range(3):
+                await bot.delete_message(callback_query.message.chat.id, data['message'][-2])
+                data['message'].pop(-2)
+    else:
+        async with state.proxy() as data:
+            for i in range(2 * (data['fail'] + 1)):
+                await bot.delete_message(callback_query.message.chat.id, data['message'][-1])
+                data['message'].pop()
 
-    async with state.proxy() as data:
-        data['message'].append(callback_query.message.message_id)
+            data['fail'] = 0
+        await NewReminder.date.set()
+        await get_reminder_text(callback_query.message, state, True)
 
 
 @dp.message_handler(regexp='!(.+): (\d{2}\.\d{2}\.\d{4}\ \d{2}\:\d{2})')
