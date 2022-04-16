2c388eb0e2fd88fa6c9627d70ee5b99d0a0cd160
RomanKuschanow <kuschanow@gmail.com>
2022-04-16 11:33:26 +0000

    error message

--- bot/handlers/errors/error_handler.py
+++ bot/handlers/errors/error_handler.py
@@ -1,4 +1,6 @@
 import traceback
+from data import config
+from loader import bot
 
 from aiogram.utils.exceptions import (Unauthorized, InvalidQueryID, TelegramAPIError,
                                       CantDemoteChatCreator, MessageNotModified, MessageToDeleteNotFound,
@@ -13,39 +15,59 @@ from utils.misc.logging import logger
 async def errors_handler(update, exception):
     if isinstance(exception, CantDemoteChatCreator):
         logger.debug('Can\'t demote chat creator')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, 'Can\'t demote chat creator')
         return True
 
     if isinstance(exception, MessageNotModified):
         logger.debug('Message is not modified')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, 'Message is not modified')
         return True
     if isinstance(exception, MessageCantBeDeleted):
         logger.debug('Message cant be deleted')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, 'Message cant be deleted')
         return True
 
     if isinstance(exception, MessageToDeleteNotFound):
         logger.debug('Message to delete not found')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, 'Message to delete not found')
         return True
 
     if isinstance(exception, MessageTextIsEmpty):
         logger.debug('MessageTextIsEmpty')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, 'MessageTextIsEmpty')
         return True
 
     if isinstance(exception, Unauthorized):
         logger.info(f'Unauthorized: {exception}')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, f'Unauthorized: {exception}')
         return True
 
     if isinstance(exception, InvalidQueryID):
         logger.exception(f'InvalidQueryID: {exception} \nUpdate: {update}')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, f'InvalidQueryID: {exception} \nUpdate: {update}')
         return True
 
     if isinstance(exception, TelegramAPIError):
         logger.exception(f'TelegramAPIError: {exception} \nUpdate: {update}')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, f'TelegramAPIError: {exception} \nUpdate: {update}')
         return True
     if isinstance(exception, RetryAfter):
         logger.exception(f'RetryAfter: {exception} \nUpdate: {update}')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, f'RetryAfter: {exception} \nUpdate: {update}')
         return True
     if isinstance(exception, CantParseEntities):
         logger.exception(f'CantParseEntities: {exception} \nUpdate: {update}')
+        for admin_id in config.ADMINS:
+            await bot.send_message(admin_id, f'CantParseEntities: {exception} \nUpdate: {update}')
         return True
 
     try:
@@ -54,3 +76,5 @@ async def errors_handler(update, exception):
         exception_traceback = traceback.format_exc()
 
     logger.exception(f'Update: {update} \n{exception_traceback}')
+    for admin_id in config.ADMINS:
+        await bot.send_message(admin_id, f'Update: {update} \n{exception_traceback}')
