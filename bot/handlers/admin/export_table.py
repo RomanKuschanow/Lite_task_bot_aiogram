e7b421cf1ba9400489b29e4c6df025b472089d83
RomanKuschanow <kuschanow@gmail.com>
2022-05-06 16:21:25 +0300

    refactor

--- bot/handlers/admin/export_table.py
+++ bot/handlers/admin/export_table.py
@@ -26,10 +26,10 @@ async def select_table(callback_query, session):
     with open(file_path, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
 
-        writer.writerow(['id', 'first_name', 'last_name', 'username', 'is_vip', 'language', 'created_at'])
+        writer.writerow(['id', 'first_name', 'last_name', 'username', 'is_vip', 'referral_id', 'language', 'created_at'])
 
         for user in users:
-            writer.writerow([user.id, user.first_name, user.last_name, user.username, user.is_vip, user.language, user.created_at])
+            writer.writerow([user.id, user.first_name, user.last_name, user.username, user.is_vip, user.referal_id, user.language, user.created_at])
 
     text_file = InputFile(file_path, filename='users.csv')
     await callback_query.message.answer_document(text_file, caption=_('Всего пользователей: {count}').format(count=count))
