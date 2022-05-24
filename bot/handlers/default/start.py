b961be00e57f5cd2380225fff59a26ba81934ad2
RomanKuschanow <kuschanow@gmail.com>
2022-05-24 11:05:56 +0300

    activate web app on start

--- bot/handlers/default/start.py
+++ bot/handlers/default/start.py
@@ -4,13 +4,20 @@ from aiogram.types import Message
 from bot.keyboards.inline import get_language_inline_markup
 from bot.states import Start
 from loader import dp, _
+import time
+
+from aiogram.types import Message, WebAppInfo, MenuButtonWebApp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: Message):
-
     await Start.lang.set()
 
     text = _('Выберите свой язык')
 
     await message.answer(text, reply_markup=get_language_inline_markup())
+
+    web_app_uri = 'https://lite-task-bot-aiogram.vercel.app/NewReminder'
+    web_app_uri += '?time=' + str(time.time())
+
+    await bot.set_chat_menu_button(message.chat.id, MenuButtonWebApp(text='+New', web_app=WebAppInfo(url=web_app_uri)))
